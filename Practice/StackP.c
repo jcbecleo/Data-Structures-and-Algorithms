df585b8e7956cd7d52393bd25d8ff95e7cfc1d0a
Joss Ecleo <ecleojosscharyborj@gmail.com>
2023-03-28 21:52:34 +0800

    practice files

--- Practice/StackP.c
+++ Practice/StackP.c
@@ -6,16 +6,29 @@ typedef struct{
     int top;
 }Stack;
 
+void initStack(Stack* S);
+void push(Stack* S, char data);
+int pop(Stack* S);
+void display(Stack S);
+int isFull(Stack S);
+int isEmpty(Stack S);
+
 int main(){
     Stack S;
+    initStack(&S);
+    ]
     return 0;
+}
+
+void initStack(Stack* S){
+    S->top = -1;
 }
 
 void push(Stack* S, char data){
     if(S->top == 0)
     printf("Stack is full.");
 
-    S->elem[++(S->top)];
+    S->elem[++(S->top)] = data;
 }
 
 int pop(Stack* S){
@@ -25,9 +38,17 @@ int pop(Stack* S){
     return S->elem[(S->top)--];
 }
 
-void  display(Stack S){
+void display(Stack S){
     int i;
     for(i=0;i<=MAX-1;i++){
-        printf("%d ", S.elem[S.top]);
+        printf("%d ", S.elem[i]);
     }
+}
+
+int isFull(Stack S){
+    (S.top == 0) ? 1 : 0;
+}
+
+int isEmpty(Stack S){
+    (S.top == MAX-1) ? 1 : 0;
 }
